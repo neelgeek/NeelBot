c6d4c2c0b2ace98fd58dff137d1263e5e3ae1c66
neelgeek <neelbhave91@gmail.com>
2018-04-21 14:26:31 +0530

    Improved logging and port fix

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 const Telegraf = require('telegraf')
-
+const express = require('express');
 const bot = new Telegraf('407966345:AAE8xrjYPYCiauSFflbjr9h1LTYk8iqvWHU');
+
+const app = express();
 
 bot.start((ctx) => ctx.reply('Welcome to the Neel Bot'));
 
 bot.command('about', ctx => {
     ctx.reply("Hello I am Neel, I am an Enginnering Student. I love to code.");
-    console.log(ctx.update);
+    LogCtx(ctx);
 });
 
 bot.command('github', ctx => {
     ctx.reply('Hey check this project on my github - github.com/neelgeek/NeelBot');
-    console.log(ctx.update);
+    LogCtx(ctx);
 })
 
 bot.on("text", ctx => {
     ctx.reply('Hey,I dont support this feature still,but I am working on it !😎');
-    console.log(ctx.update);
+    LogCtx(ctx);
 })
 
 bot.on("sticker", ctx => {
     ctx.reply('Hey,I dont support this feature still,but I am working on it !😎');
+    LogCtx(ctx);
 })
 
 bot.catch((err) => {
     console.log('Ooops', err)
 })
 
+
+function LogCtx(ctx) {
+    console.log(ctx.message.text + " Sent by " + ctx.message.from.username);
+}
+
+
+var port = process.env.PORT | 8000;
+app.listen(port, function() {
+    console.log("Running on Port " + port);
+});
 
 bot.startPolling();
