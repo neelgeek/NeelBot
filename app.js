ff24d382055785954e48e794cc1e0c5592bf6346
neelgeek <neelbhave91@gmail.com>
2018-04-21 14:01:05 +0530

    Text and sticker handling

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ bot.start((ctx) => ctx.reply('Welcome to the Neel Bot'));
 
 bot.command('about', ctx => {
     ctx.reply("Hello I am Neel, I am an Enginnering Student. I love to code.");
+    console.log(ctx);
 });
 
 bot.command('github', ctx => {
     ctx.reply('Hey check out my github - github.com/neelgeek');
+    console.log(ctx);
 })
 
+bot.on("text", ctx => {
+    ctx.reply('Hey,I dont support this feature still,but I am working on it !😎');
+    console.log(ctx);
+})
+
+bot.on("sticker", ctx => {
+    ctx.reply('Hey,I dont support this feature still,but I am working on it !😎');
+})
 
 
 bot.startPolling();
