9c0399a44ba4ee4bf05861670bf6c60c23b283fc
neelgeek <neelbhave91@gmail.com>
2018-04-21 14:45:24 +0530

    Port Fixing

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf')
 const http = require('http');
-const bot = new Telegraf('407966345:AAE8xrjYPYCiauSFflbjr9h1LTYk8iqvWHU');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 //const bot = new Telegraf(process.env.BOT_TOKEN);
 
 
